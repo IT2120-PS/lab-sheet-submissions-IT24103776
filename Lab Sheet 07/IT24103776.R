56442b09a42a1d53ae2dcc0e0ba4a1cbd8523a3f
IT24103776 <IT24103776@my.sliit.lk>
2025-09-25 10:20:15 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103776.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\IT24103776\\Desktop\\IT24103776")
+punif(upper, min=a, max=b) - punif(lower, min=a, max=b)
+
+
+#Q1
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+#Q2
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+#Q3 - (i)
+1 - pnorm(130, mean=100, sd=15)
+
+#Q3 - (ii)
+qnorm(0.95, mean=100, sd=15)
+
+
